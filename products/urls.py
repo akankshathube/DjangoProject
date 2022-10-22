c5f283054e0ba786616e477ac67c19b949039d67
akankshathube <91462088+akankshathube@users.noreply.github.com>
2022-10-22 20:37:40 +0530

    Products

---
+++ products/urls.py
@@ -0,0 +1,20 @@
+#includ<stdio.h>
+{
+main()
+int a[15],i,loc,n,key;
+printf("\n how mony numbers");
+scanf("%d",& n);
+printf("\n enter array numbers");
+for(i=0;i<n;i++);
+scanf("%d"&a[i]);
+printf("\n entor numbers to besearch");
+scanf("%d",& key)
+function loc- linearssearch (a,n,key);
+if(loc!=-1)
+printf("\n element itpresent at location %d", loc);
+else printf("\n element of not present");
+}
+best case- when the element is present at first 
+
+
+
